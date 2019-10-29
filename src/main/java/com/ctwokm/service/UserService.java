be454f041e5ebf1585a50c0920be2ea82d79e386
Ctwokm <Ctwokm@LAPTOP-MVH01F53>
2019-10-29 09:55:38 +0800

    Redis缓存更新

--- src/main/java/com/ctwokm/service/UserService.java
+++ src/main/java/com/ctwokm/service/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.ctwokm.dao.UserDAO;
@@ -11,6 +13,7 @@ import com.ctwokm.pojo.User;
 import com.ctwokm.system.vo.Grid;
 
 @Service
+//@CacheConfig(cacheNames = "sys_user")
 public class UserService {
 	@Resource
 	private UserDAO userDAO;
@@ -21,6 +24,7 @@ public class UserService {
 	 * @param loginName
 	 * @return
 	 */
+//	@Cacheable(key = "'loginName'.concat(#loginName)")
 	public User findByLoginName(String name) {
 		return userDAO.findByLoginName(name);
 	};
@@ -41,35 +45,36 @@ public class UserService {
 	 * @param loginName
 	 * @return
 	 */
-/*	public String getPasswordByLoginName(String loginName) {
-		return userDAO.getPasswordByLoginName(loginName);
-	};*/
-	
+	/*
+	 * public String getPasswordByLoginName(String loginName) { return
+	 * userDAO.getPasswordByLoginName(loginName); };
+	 */
+
 	/**
 	 * 必要时可以限定一下查询的条数用以分页
+	 * 
 	 * @return
 	 */
-	public Grid findList(){
-        Grid grid = new Grid();
-        
-        
-        List<User> users = userDAO.listUsers(null);
-        
-        grid.setRows(users);
-        grid.setTotal(users.size());
+	public Grid findList() {
+		Grid grid = new Grid();
 
-        return grid;
-    }
-	
-	public User getByOpenid(String openid){
-        //这里假装是通过openid从数据库中获取到用户信息
-        User user = new User();
-        user.setName("张三");
-        user.setLoginName("zhangsan");
-        user.setPhone("18888888888");
-        user.setId(1);
-        user.setLoginFlag("1");
+		List<User> users = userDAO.listUsers(null);
 
-        return user;
-    }
+		grid.setRows(users);
+		grid.setTotal(users.size());
+
+		return grid;
+	}
+
+	public User getByOpenid(String openid) {
+		// 这里假装是通过openid从数据库中获取到用户信息
+		User user = new User();
+		user.setName("张三");
+		user.setLoginName("zhangsan");
+		user.setPhone("18888888888");
+		user.setId(1);
+		user.setLoginFlag("1");
+
+		return user;
+	}
 }
